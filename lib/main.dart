dd0473ad536f097540d97bfcd2e2eb3d9d241daa
KiranJoshi-Flutter <KiranJoshi-Flutter@users.noreply.github.com>
2021-07-10 08:08:22 +0545

    youtube

--- lib/main.dart
+++ lib/main.dart
@@ -5,109 +5,215 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height: 100.0,
+              padding: EdgeInsets.only(
+                top: 41.0,
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: 250.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Icon(Icons.arrow_back),
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.only(
+                              left: 10.0,
+                              right: 5.0,
+                            ),
+                            height: 35.0,
+                            color: Color(0xFFF3F3F3),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'relaxing music',
+                                  style: TextStyle(
+                                    fontSize: 18.0,
+                                  ),
+                                ),
+                                Icon(Icons.close),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: double.infinity,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(Icons.mic),
+                          Icon(Icons.cast),
+                          Icon(Icons.more_vert),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 200.0,
+                              width: double.infinity,
+                              color: Colors.black,
+                              child: Image.asset(
+                                'assets/titleImages/1.jpg',
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: ListTile(
+                                  leading: Icon(Icons.person),
+                                  title: Text(
+                                    'Study Music 24/7, Focus, Meditation, Concentration Music, Relaxing Music, Calm...',
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  subtitle: Text(
+                                    'Yellow Brick Cinema - Relaxing Music - 1.2K watching',
+                                    style: TextStyle(
+                                      fontSize: 10.0,
+                                    ),
+                                  ),
+                                  trailing: Icon(Icons.more_vert),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 200.0,
+                              width: double.infinity,
+                              color: Colors.black,
+                              child: Image.asset(
+                                'assets/titleImages/maxresdefault.jpg',
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                child: ListTile(
+                                  leading: Icon(Icons.person),
+                                  title: Text(
+                                    'Study Music 24/7, Focus, Meditation, Concentration Music, Relaxing Music, Calm...',
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  subtitle: Text(
+                                    'Yellow Brick Cinema - Relaxing Music - 1.2K watching',
+                                    style: TextStyle(
+                                      fontSize: 10.0,
+                                    ),
+                                  ),
+                                  trailing: Icon(Icons.more_vert),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.yellow,
+                      ),
+                      child: Image.asset(
+                        'assets/titleImages/3.jpg',
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 60.0,
+              decoration: BoxDecoration(),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(
+                    Icons.home,
+                    size: 32.0,
+                  ),
+                  Icon(
+                    Icons.explore_outlined,
+                    size: 32.0,
+                  ),
+                  Icon(
+                    Icons.add_circle_outline_outlined,
+                    size: 34.0,
+                  ),
+                  Icon(
+                    Icons.subscriptions_outlined,
+                    size: 32.0,
+                  ),
+                  Icon(
+                    Icons.library_add_check_outlined,
+                    size: 32.0,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
